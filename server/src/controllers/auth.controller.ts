6c369e189001ba2d0ae4f8a170ac289fa9068033
HudsonAdjetey <adjetey545@gmail.com>
2024-08-20 08:44:11 -0700

    controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
 import prisma from "../db/prisma";
+import bcrypt, { genSalt } from "bcryptjs";
+import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken";
 
 const login = asyncHandler(async (req: Request, res: Response, next) => {
   try {
@@ -25,20 +28,25 @@ const logout = asyncHandler(async (req: Request, res: Response, next) => {
 
 // type for req body
 
-interface SignupRequest {
-  username: string;
-  password: string;
-  email: string;
-  confirmPassword: string;
-  gender: string;
+enum Gender {
+  MALE = "MALE",
+  female = "FEMALE",
+  other = "OTHER",
 }
 
 const signup = asyncHandler(async (req: Request, res: Response, next) => {
   try {
-    // Ensure that req.body is typed appropriately
-    const { username, password, email, confirmPassword, gender } =
-      req.body as SignupRequest;
-
+    const genderEnumValue: Gender =
+      Gender[req.body.gender.toUpperCase() as keyof typeof Gender];
+    const { username, password, email, confirmPassword, gender, fullName } =
+      req.body as {
+        username: string;
+        password: string;
+        email: string;
+        confirmPassword: string;
+        gender: typeof genderEnumValue;
+        fullName: string;
+      };
     // Check if the fields are not present
     if (!username || !password || !email || !confirmPassword || !gender) {
       res.status(400).json({
@@ -64,7 +72,40 @@ const signup = asyncHandler(async (req: Request, res: Response, next) => {
       });
       return;
     }
-  } catch (error) {
+    const genSalt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, genSalt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        gender,
+        password: hashedPassword,
+        fullName,
+        profilePic: gender === Gender.MALE ? boyProfilePic : girlProfilePic,
+      },
+    });
+
+    if (newUser) {
+      // generate token
+      generateToken(newUser.id, res);
+
+      res.json({
+        success: true,
+        message: "Signup successful",
+        user: {
+          id: newUser.id,
+          username: newUser.username,
+          profilePic: newUser.profilePic,
+        },
+      });
+    } else {
+      res.status(400).json({ success: false, message: "Signup failed" });
+      return;
+    }
+  } catch (error: any) {
     console.error(error);
     res.status(500).json({ success: false, message: "Server error" });
     next(error);
---
+++ server/src/utils/generateToken.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { Response } from "express";
+
+const generateToken = (userId: string, res: Response): {} | void => {
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    process.env.JWT_SECRET!,
+    {
+      expiresIn: "15d", // 1 hour
+    }
+  );
+  res.cookie("access", token, {
+    httpOnly: true,
+    maxAge: 15 * 24 * 60 * 60,
+    sameSite: "strict", //
+    secure: process.env.NODE_ENV! === "production", // true for production
+  });
+
+  return token;
+};
+
+export default generateToken;
