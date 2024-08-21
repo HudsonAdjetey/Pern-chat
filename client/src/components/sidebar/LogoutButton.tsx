793ee38c1dd22558ede3843b8dfcacb6f81169a1
HudsonAdjetey <adjetey545@gmail.com>
2024-08-21 06:58:11 -0700

    working folder -> errors

---
+++ client/src/components/sidebar/LogoutButton.tsx
@@ -0,0 +1,14 @@
+import { LogOut } from "lucide-react";
+// import useLogout from "../../hooks/useLogout";
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+      <LogOut
+        className="w-6 h-6 text-white cursor-pointer"
+        onClick={() => {}}
+      />
+    </div>
+  );
+};
+export default LogoutButton;
